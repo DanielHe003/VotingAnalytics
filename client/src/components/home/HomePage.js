d30d46e7a5df1d5976965bb1c857576542b85be4
Tanmay Singhal <43653881+tan-june@users.noreply.github.com>
2024-09-27 12:45:48 -0400

    In Progress 2

--- client/src/components/home/HomePage.js
+++ client/src/components/home/HomePage.js
@@ -1,22 +1,11 @@
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faChartLine, faInfoCircle } from '@fortawesome/free-solid-svg-icons';
-import { Box, Typography } from '@mui/material';
 
 const HomePage = ({ navLinks, activeSection, setActiveSection }) => {
   return (
     <div style={{ padding: '0 10%' }}>
       <h1>Home Page In Progress</h1>
 
-      <Box display="flex" alignItems="center" marginTop={4}>
-        <FontAwesomeIcon icon={faChartLine} size="2x" style={{ marginRight: '8px' }} />
-        <Typography variant="h5">State Analysis</Typography>
-      </Box>
-
-      <Box display="flex" alignItems="center" marginTop={2}>
-        <FontAwesomeIcon icon={faInfoCircle} size="2x" style={{ marginRight: '8px' }} />
-        <Typography variant="h5">Info</Typography>
-      </Box>
+  
     </div>
   );
 };
